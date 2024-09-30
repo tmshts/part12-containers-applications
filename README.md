e5f3a6d28f127036fdc1e0f0ed42c1d1bccccd04
tmshts <tomhatas@gmail.com>
2024-09-30 10:27:46 +0200

    added some exercises

---
+++ README.md
@@ -0,0 +1,7 @@
+# part12-containers-applications
+
+This repository is dedicated to the Part 12 Containers of the course Deep Dive Into Modern Web Development.
+
+More information related to this course can be found [here](https://fullstackopen.com/en/part0/general_info).
+
+More information related to the Part 12 Containers can be found [here](https://fullstackopen.com/en/part12).
---
+++ todo-app/todo-backend/routes/todos.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { Todo } = require('../mongo')
+const router = express.Router();
+
+/* GET todos listing. */
+router.get('/', async (_, res) => {
+  const todos = await Todo.find({})
+  res.send(todos);
+});
+
+/* POST todo to listing. */
+router.post('/', async (req, res) => {
+  const todo = await Todo.create({
+    text: req.body.text,
+    done: false
+  })
+  res.send(todo);
+});
+
+const singleRouter = express.Router();
+
+const findByIdMiddleware = async (req, res, next) => {
+  const { id } = req.params
+  req.todo = await Todo.findById(id)
+  if (!req.todo) return res.sendStatus(404)
+
+  next()
+}
+
+/* DELETE todo. */
+singleRouter.delete('/', async (req, res) => {
+  await req.todo.delete()  
+  res.sendStatus(200);
+});
+
+/* GET todo. */
+singleRouter.get('/' , async (req, res) => {
+  // const { id } = req.params
+  // const todo = await Todo.findById(id)
+  res.send(req.todo);
+  // res.sendStatus(405); // Implement this
+});
+
+/* PUT todo. */
+singleRouter.put(`/`, async (req, res) => {
+
+  const update_todo = {
+                          text: req.body.text,
+                          done: req.body.done
+                      }
+  const updated_todo = await Todo.findByIdAndUpdate(req.todo.id, update_todo);
+  res.send(updated_todo)
+  //res.sendStatus(405); // Implement this
+});
+
+router.use('/:id', findByIdMiddleware, singleRouter)
+
+
+module.exports = router;
